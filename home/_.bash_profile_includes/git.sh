73adbae964c25336737425b276be3292387947c7
Steve Schwartz <steve@alfajango.com>
2012-06-29 00:40:43 -0400

    Added gitnotes alias to print out git log tasks

--- home/_.bash_profile_includes/git.sh
+++ home/_.bash_profile_includes/git.sh
@@ -19,3 +19,4 @@ alias grs="git remote -v"
 alias grl="git remote -v" # I forget it's "show" and not "list"
 alias grh="git reset HEAD"
 alias gdi="git diff"
+alias gitnotes='git log --pretty=format:"* %s"'
