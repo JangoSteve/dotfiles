ce2750da48522121d2e2cbb6478c4e1cdd9d15a8
Steve Schwartz <steve@alfajango.com>
2013-01-21 09:02:15 -0500

    Moved and fixed custom git init bootstrap to avoid command name conflict.

---
+++ home/_.bash_profile_includes/aj_git_init.sh
@@ -0,0 +1,8 @@
+aj_git_init(){
+  git init
+  echo "web: bundle exec rails server" > Procfile.sample
+  cp Procfile.sample Procfile
+  echo "port: 5000" > .foreman
+  echo "ENV: development" > .env
+  echo ".foreman\n.env\nProcfile" > .gitignore
+}
--- home/_.bash_profile_includes/gitinit.sh
+++
@@ -1,6 +0,0 @@
-git init
-echo "web: bundle exec rails server" > Procfile.sample
-cp Procfile.sample Procfile
-echo "port: 5000" > .foreman
-echo "ENV: development" > .env
-echo ".foreman\n.env\nProcfile" > .gitignore
