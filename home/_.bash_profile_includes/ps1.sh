851168301cd80fa872254414eb43843e13fa91a4
Steve Schwartz <steve@alfajango.com>
2013-03-04 01:37:51 -0500

    Cleaned up formatting of ps1.sh and removed some unnecessary stuff. Rename term tab titles.

--- home/_.bash_profile_includes/ps1.sh
+++ home/_.bash_profile_includes/ps1.sh
@@ -15,19 +15,10 @@
 # @jangosteve
 
 ###########################################
-# Fill with minuses
-
-# (this is recalculated every time the prompt is shown in function prompt_command):
-
-fill="--- "
-
 
 reset_style='\[\033[00m\]'
-
 status_style=$reset_style'\[\033[0;90m\]' # gray color; use 0;37m for lighter color
-
 prompt_style=$reset_style
-
 command_style=$reset_style'\[\033[1;29m\]' # bold black
 
 # Prompt variable:
@@ -50,8 +41,6 @@ export -f parse_last_status
 export CLICOLOR=1
 export PS1="$status_style"'$fill \d \t\n'"\$(parse_last_status)\W \[\033[33m\]\$(parse_git_branch)\[\033[00m\]$\[\033[00m\] "
 export SUDO_PS1='\[\e[0;31m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[0;31m\]\$ \[\e[0m\]'
-#PS1="$status_style"'$fill \t\n'"$prompt_style"'${debian_chroot:+($debian_chroot)}\u@\h:\w\$'"$command_style "
-
 
 # Reset color for command output
 
@@ -59,46 +48,28 @@ export SUDO_PS1='\[\e[0;31m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[0;31m\]\$ \[
 
 trap 'echo -ne "\033[00m"' DEBUG
 
-
 function prompt_command {
+  # Fill with minuses
+  # (this is recalculated every time the prompt is shown in function prompt_command):
 
+  # create a $fill of all screen width minus the time string and a space:
+  let fillsize=${COLUMNS}-20
+  fill=""
+  while [ "$fillsize" -gt "0" ]
+  do
+    fill="-$fill" # fill with underscores to work on
+    let fillsize=${fillsize}-1
+  done
 
-# create a $fill of all screen width minus the time string and a space:
-
-let fillsize=${COLUMNS}-20
-
-fill=""
-
-while [ "$fillsize" -gt "0" ]
-
-do
-
-  fill="-${fill}" # fill with underscores to work on
-
-  let fillsize=${fillsize}-1
-
-done
-
-
-# If this is an xterm set the title to user@host:dir
-
-case "$TERM" in
-
-  xterm*|rxvt*)
-
-  bname=`basename "${PWD/$HOME/~}"`
-
-  echo -ne "\033]0;${bname}: ${USER}@${HOSTNAME}: ${PWD/$HOME/~}\007"
-
-  ;;
-
-  *)
-
-  ;;
-
-esac
-
-
+  # If this is an xterm set the title to name of dir
+  case "$TERM" in
+    xterm*|rxvt*)
+    bname=`basename "${PWD/$HOME/~}"`
+    echo -ne "\033]0;${bname}\007"
+    ;;
+    *)
+    ;;
+  esac
 }
 
 PROMPT_COMMAND=prompt_command
