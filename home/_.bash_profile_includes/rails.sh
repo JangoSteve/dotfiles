cc2dde69171a56c3268390959e29ccca07be3eef
Steve Schwartz <steve@alfajango.com>
2013-04-10 16:54:15 -0400

    Added start bash command to detect Procfile.dev or Procfile and start foreman accordingly or fallback to rails server.

--- home/_.bash_profile_includes/rails.sh
+++ home/_.bash_profile_includes/rails.sh
@@ -3,3 +3,21 @@ alias rc="rails c"
 alias ber="bundle exec rspec"
 alias test="ruby -Itest"
 alias bet="bundle exec rake test"
+
+start(){
+  if [ -f ./Procfile.dev ];
+  then
+    echo "Procfile.dev detected"
+    echo "foreman start -f Procfile.dev"
+    foreman start -f Procfile.dev
+  elif [ -f ./Procfile ];
+  then
+    echo "Procfile detected"
+    echo "foreman start"
+    foreman start
+  else
+    echo "No Procfiles detected, falling back to rails"
+    echo "rails server"
+    rails server
+  fi
+}
