2a710e331e813fc3b66a84f828ffa691cbb15431
Steve Schwartz <steve@alfajango.com>
2015-07-22 12:40:25 -0400

    Added alias to say 'migration complete' after runing rails migrations.

--- home/_.bash_profile_includes/rails.sh
+++ home/_.bash_profile_includes/rails.sh
@@ -3,3 +3,10 @@ alias rc="rails c"
 alias ber="bundle exec rspec"
 alias test="ruby -Itest"
 alias bet="bundle exec rake test"
+rake() {
+    if [[ $@ == "db:migrate" ]]; then
+        command rake db:migrate && say -vz "migration complete"
+    else
+        command rake "$@"
+    fi
+}
