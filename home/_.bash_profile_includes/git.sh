8b07642529296e50c5f993f48c5de37a704be8eb
Steve Schwartz <steve@alfajango.com>
2017-03-24 13:52:08 -0400

    Use got completion functions for git aliases.

--- home/_.bash_profile_includes/git.sh
+++ home/_.bash_profile_includes/git.sh
@@ -1,10 +1,14 @@
 alias gco="git checkout"
+__git_complete gco _git_checkout
+#complete -o default -o nospace -F _git_checkout gco
 alias gst="git status"
 alias gcm="git commit -m"
 alias gca="git commit --amend"
 alias gpr="git pull --rebase"
 alias gpl="git pull"
 alias gpu="git push"
+__git_complete gpu _git_push
+#complete -o default -o nospace -F _git_push gpu
 alias gri="git rebase -i"
 alias gre="git rebase"
 alias gad="git add"
