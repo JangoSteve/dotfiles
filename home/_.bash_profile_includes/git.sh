8076fef172865e97419e0a477b04ee5539adcbc1
Steve Schwartz <steve@alfajango.com>
2017-05-25 17:27:37 -0400

    Created a bunch of new useful get aliases, inspired by https://csswizardry.com/2017/05/little-things-i-like-to-do-with-git/

--- home/_.bash_profile_includes/git.sh
+++ home/_.bash_profile_includes/git.sh
@@ -18,12 +18,21 @@ __git_complete gme _git_checkout
 alias glo="git log"
 alias gbl="git branch -l"
 alias gbla="git branch -a"
+alias gbw='git for-each-ref --count=10 --sort=-committerdate refs/heads/ --format="%(refname:short)"'
 alias gfe="git fetch"
 alias gf="git fetch" # slightly shorter version
 alias grs="git remote -v"
 alias grl="git remote -v" # I forget it's "show" and not "list"
 alias grh="git reset HEAD"
-alias gdi="git diff"
-alias gitsum='git log --pretty=format:"* %s" --author `git config user.email`'
+alias gdi="git diff --word-diff"
+alias gitsum='git log --pretty=format:"* %s" --author `git config user.email`' #myself
+alias gitover="git log --all --oneline --no-merges" # everyone, aka git "overview"
+alias gittoday="git log --since=00:00:00 --all --no-merges --oneline --author `git config user.email`"
 alias gsu="gitsum"
 alias gtr="git log --graph --pretty=oneline --abbrev-commit"
+alias gitgraph="git log --graph --all --decorate --stat --date=iso"
+alias gsl="git shortlog -sn"
+alias gitchange="git log --oneline --no-merges `git describe --abbrev=0 --tags`..HEAD"
+alias gitupstream=`git rev-parse --abbrev-ref --symbolic-full-name @{u}`
+alias gitupcoming="git fetch \$gitupstream && git log --oneline --no-merges HEAD..\$gitupstream"
+alias gitlocal="git fetch \$gitupstream && git log --oneline --no-merges \$gitupstream..HEAD"
